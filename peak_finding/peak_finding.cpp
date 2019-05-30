302364a89fa2acc0c65c2e2be15058345262248f
lazybing <libinglimit@gmail.com>
2019-05-30 03:23:03 -0700

    add the peak finer of 2D implementation of C++

--- peak_finding/peak_finding.cpp
+++ peak_finding/peak_finding.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int peak_finder_1d(int array[], int start, int end){
@@ -12,9 +13,51 @@ int peak_finder_1d(int array[], int start, int end){
     return middle;
 }
 
+// find the maximum in column "mid"
+int findMax(int arr[][4], int rows, int mid, int &max)
+{
+    int max_idx = 0;
+    for(int i = 0; i < rows; i++)    
+	if(arr[i][mid] >= max){
+	    max = arr[i][mid];
+	    max_idx = i;
+	}
+    return max_idx;
+}
+
+//Function to find a peak element in 2d array
+int peak_finder_2d(int arr[][4], int rows, int columns, int mid)
+{
+    int max = 0;
+    int max_idx = findMax(arr, rows, mid, max);
+
+    if ( mid == 0 || mid == columns - 1 )
+	return max;
+
+    if ( max >= arr[max_idx][mid - 1] &&
+	 max >= arr[max_idx][mid + 1])
+	 return max;
+
+    if (max < arr[max_idx][mid - 1])
+	return peak_finder_2d(arr, rows, columns, mid -ceil((double)mid / 2));
+
+    if (max < arr[max_idx][mid + 1])
+	return peak_finder_2d(arr, rows, columns, mid + ceil((double)mid / 2));
+
+    return max;
+}
+
 int main(int argc, char **argv){
 
     int array[] = {0, 3, 5, 8, 7, 9, 2, 13};
     cout << "the peak position " << peak_finder_1d(array, 0, sizeof(array) / sizeof(array[0]) - 1) << endl;
 
+    int array2d[][4] = {{10, 8, 10, 10},
+		       {14, 13, 12, 11},
+		       {15, 9, 11, 21},
+		       {16, 17, 19, 20}};
+    cout << "the peak number in 2d array " << peak_finder_2d(array2d, 4, 4, 2) << endl;
+
+    return 0;
 }
+
--- peak_finding/peak_finding.md
+++ peak_finding/peak_finding.md
@@ -17,4 +17,15 @@ Position 2 is a peak if and only if b >= a and b >=c, Position 9 is a peak if i 
 
 a is a 2D-peak if a >= b, a >= d, a >= c, a >= e
 
+This problem is mainly an extension of Find a peak element in 1D array. We apply similar binary search based solution here.
+
+1. Consider mid column and find maximum element in it.
+2. Let index of mid column be 'mid', value of maximum element in mid column be 'max' and maximum element be at mat[max_index][mid].
+3. If max >= A[index][mid-1] && max >= A[index][pick+1], max is a peak, return max.
+4. If max < mat[max_index][mid-1], recur for left half of matrix.
+5. If max < mat[max_index][mid+1], recur for right half of matrix.
  
+### References
+1. [C++ Program to Find the peak element of an array using Binary Search approach](https://www.sanfoundry.com/cpp-program-find-peak-element-array-binary-search/)
+2. [Find a peak element in a 2D array](https://www.geeksforgeeks.org/find-peak-element-2d-array/)
+3. [MIT course Algorithmic Thinking, Peak Finding](https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/lecture-1-algorithmic-thinking-peak-finding/)
