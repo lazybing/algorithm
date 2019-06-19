f5e42c393f0c42059686ff0cde5ea99bc0b47949
libing <libinglimit@gmail.com>
2019-06-19 16:34:29 -0700

    add the invert binary tree solution

---
+++ leetcode226_invert_binary_tree/inverttree.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+        TreeNode* invertTree(TreeNode* root){
+            if (!root) return NULL;
+
+            swap(root->left, root->right);
+            invertTree(root->left);
+            invertTree(root->right);
+
+            return root;
+        }
+};
