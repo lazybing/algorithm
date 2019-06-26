83a8ef4318f5730c52f54db8c99924fe732a4823
libing <libinglimit@gmail.com>
2019-06-25 17:02:41 -0700

    add another solution for get sum

--- leetcode371_sum_of_two_integers/getsum.cpp
+++ leetcode371_sum_of_two_integers/getsum.cpp
@@ -1,3 +1,21 @@
+class Solution1{
+    public:
+        int getSum (int a, int b) {
+            int carray = a ^ b;
+            int c = a & b;
+
+            while (c != 0) {
+                c = (unsigned int)c << 1;
+                int carray_prim = carray ^ c;
+                c = carray & c;
+                carray = carray_prim;
+            }
+
+            return carray;
+        }
+};
+
+
 class Solution {
     public:
         int getSum(int a, int b) {
