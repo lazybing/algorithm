494d83712ddbb7445d9156bb435274c8ede87a74
lazybing <libinglimit@gmail.com>
2019-07-18 19:24:01 -0700

    first bad version solution

---
+++ leetcode278_first_bad_version/firstbadversion.cpp
@@ -0,0 +1,16 @@
+bool isBadVersion(int version);
+
+class Solution {
+    public:
+        int firstBadVersion(int n) {
+            int lower = 1, upper = n, mid;
+            while (lower < upper) {
+                mid = lower + (upper - lower) / 2;
+                if (!isBadVersion(mid))
+                    lower = mid + 1;
+                else
+                    upper = mid;
+            }
+            return lower;
+        }
+};
