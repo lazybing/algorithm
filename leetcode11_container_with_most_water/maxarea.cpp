93f6dff068bc29fcfffa968cdc6406f191c640eb
lazybing <libinglimit@gmail.com>
2019-08-02 09:13:28 -0700

    another solution for container with most water

--- leetcode11_container_with_most_water/maxarea.cpp
+++ leetcode11_container_with_most_water/maxarea.cpp
@@ -1,3 +1,4 @@
+//solution one
 class Solution {
 public:
     int maxArea(vector<int>& height){
@@ -12,4 +13,25 @@ public:
 
         return res;
     }
+};
+
+//solution two
+class Solution {
+    public:
+        int maxArea(vector<int>& height){
+            int res  = 0;
+            int area = 0;
+            int left = 0, right = height.size() - 1;
+
+            while (left < right) {
+                area = (right - left) * min(height[right], height[left]);
+                res  = max(res, area);
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return res;
+        }
 };
