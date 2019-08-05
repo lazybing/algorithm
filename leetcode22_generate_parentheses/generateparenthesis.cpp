058a3d309f03cc2d6fea7e750697c75f59634969
lazybing <libinglimit@gmail.com>
2019-08-04 23:10:40 -0700

    generate parentheses solution

---
+++ leetcode22_generate_parentheses/generateparenthesis.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        void addingpar(vector<string>&v, string str, int n, int m){
+            if (n == 0 && m == 0){
+                v.push_back(str);
+                return;
+            }
+
+            if (n > 0) addingpar(v, str + "(", n - 1, m + 1);
+            if (m > 0) addingpar(v, str + ")", n, m + 1);
+        }
+
+
+        vector<string> generateParenthesis(int n){
+            vector<string> res;
+            addingpar(res, "", n, 0);
+            return res;
+        }
+};
