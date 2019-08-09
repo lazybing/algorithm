e03330d48e8ad17e75aa8a524b22e8e7b2791609
lazybing <libinglimit@gmail.com>
2019-08-09 08:19:00 -0700

    rotate list solution

---
+++ leetcode61_rotate_list/rotateright.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    public:
+        ListNode * rotateRight(ListNode *head, int k) {
+            if (!head) return head;
+
+            int len = 1;
+
+            ListNode *newH, *tail;
+            newH = tail = head;
+
+            while (tail->next) {
+                tail = tail->next;
+                len++;
+            }
+            tail->next = head;
+
+            if (k %= len) {
+                for (auto i = 0; i < len - k; i++)
+                    tail = tail->next;
+            }
+            newH = tail->next;
+            tail->next = NULL;
+
+            return newH;
+        }
+};
