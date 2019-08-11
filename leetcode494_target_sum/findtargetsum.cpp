8f6eeea71f2489d204ceba052d971df9caeb733e
lazybing <libinglimit@gmail.com>
2019-08-11 08:55:55 -0700

    a dp solution

--- leetcode494_target_sum/findtargetsum.cpp
+++ leetcode494_target_sum/findtargetsum.cpp
@@ -11,3 +11,20 @@ class Solution {
             return find(0, nums, S);
         }
 };
+
+class Solution {
+    public:
+        int findTargetsumWays(vector<int>& nums, int S) {
+            int sum = accumulate(nums.begin(), nums.end(), 0);
+            if (sum < S || (sum -S) % 2) return 0;
+            int target = (sum - S) / 2;
+            vector<int> dp(target + 1, 0);
+            dp[0] = 1;
+            for (int i = 0; i < nums.size(); i++) {
+                for (int j = target; j >= nums[i]; j--)
+                    dp[j] += dp[j - nums[i]];
+            }
+
+            return dp[target];
+        }
+};
