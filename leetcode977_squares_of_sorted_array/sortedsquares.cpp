7bda9dc7d9dc79683c1f5d8c38783c94254fb2d5
lazybing <libinglimit@gmail.com>
2019-08-13 05:03:28 -0700

    squares of sorted array solution

--- leetcode977_squares_of_sorted_array/sortedsquares.cpp
+++ leetcode977_squares_of_sorted_array/sortedsquares.cpp
@@ -11,3 +11,21 @@ class Solution {
             return res;
         }
 };
+
+class Solution {
+    public:
+        vector<int> sortedSquares(vector<int>& A) {
+            int size = A.size();
+            vector<int> res(size, 0);
+            int i = 0, j = size - 1;
+
+            for (int k = size - 1; k >= 0; k--) {
+                if (abs(A[i]) > abs(A[j]))
+                    res[k] = pow(A[i++], 2);
+                else
+                    res[k] = pow(A[j--], 2);
+            }
+
+            return res;
+        }
+};
