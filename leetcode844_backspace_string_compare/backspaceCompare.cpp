a8456fe62b4cdc41475c75b5b62fd1fcb0084348
lazybing <libinglimit@gmail.com>
2019-10-17 20:07:59 -0700

    using the two pointer to implement the backspace string compare solution

--- leetcode844_backspace_string_compare/backspaceCompare.cpp
+++ leetcode844_backspace_string_compare/backspaceCompare.cpp
@@ -15,3 +15,31 @@ class Solution {
             return s_stack == t_stack;
         }
 };
+
+class Solution{
+    public:
+        bool backspaceCompare(string S, string T){
+            int i = S.size(), j = T.size();
+            int skipS = 0, skipT = 0;
+
+            while (i >= 0 || j >= 0) {
+                while (i >= 0) {
+                    if (S[i] == '#') {skipS++; i--;}
+                    else if (skipS > 0) {skipS--; i--;}
+                    else break;
+                }
+                while (j >= 0) {
+                    if (T[j] == '#'){skipT++; j--;}
+                    else if (skipT > 0) {skipT--; j--;}
+                    else break;
+                }
+                if (i >= 0 && j >= 0 && S[i] != T[j])
+                    return false;
+                if ((i >= 0) != (j >= 0))
+                    return false;
+                i--;
+                j--;
+            }
+            return true;
+        }
+};
