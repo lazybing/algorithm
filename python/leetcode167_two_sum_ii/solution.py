e66ddbd3fda53e67d9bfe1f3da7753aca958aeb8
lazybing <libinglimit@gmail.com>
2019-11-23 22:55:58 -0800

    leetcode solution two sum ii

---
+++ python/leetcode167_two_sum_ii/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    """
+    " type namber: List[int]
+    " type target: int
+    " rtype: List[int]
+    """
+    def twoSum(self, number:List[int], target: int)->List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            if (numbers[left] + numbers[right] == target):
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
+
+        return []
