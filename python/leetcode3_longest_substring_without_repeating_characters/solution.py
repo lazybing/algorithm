6332e1ecdd036c5c023c34d6e8acc80e9e9e992a
lazybing <libinglimit@gmail.com>
2019-11-28 19:11:24 -0800

    longest substring without repeating characters python solution

--- python/leetcode3_longest_substring_without_repeating_characters/solution.py
+++ python/leetcode3_longest_substring_without_repeating_characters/solution.py
@@ -1,3 +1,4 @@
+# runtime 1184ms beat 5%
 class Solution:
     def lengthOfLongestSubstring(self, s:str)->int:
         res, left, right = 0, 0, 0
@@ -14,5 +15,28 @@ class Solution:
                     right = right + 1
 
             setttings.clear()
+
+        return res
+
+# runtime 68ms beat 64%
+class Solution:
+    def lengthOfLongestSubstring(self, s:str)->int:
+        res, left, right = 0, 0, 0
+        settings = set()
+
+        while right < len(s):
+            if s[right] in settings:
+                while left < right:
+                    if s[left] == s[right]:
+                        settings.discard(s[left])
+                        left = left + 1
+                        break
+                    else:
+                        settings.discard(s[left])
+                        left = left + 1
+
+            settings.add(s[right])
+            res = max(res, right - left + 1)
+            right = right + 1
 
         return res
